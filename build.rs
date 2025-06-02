9e0f7add9a5d7cd04873da483226a58e2a1bd81d
Eugene <eizeed321@gmail.com>
2025-06-02 16:03:50 +0500

    Cleanup :)

--- build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let tesseract_dir = "D:/personal/voidhunter/3rdParty/tesseract";
-
-    // println!("cargo:rustc-env=TESSERACT_INCLUDE_PATHS={}/libtesseract.dll", tesseract_dir);
-    // println!("cargo:rustc-env=TESSERACT_LINK_PATHS={}", tesseract_dir);
-    // println!("cargo:rustc-env=TESSERACT_LINK_LIBS=libtesseract.dll");
-}
